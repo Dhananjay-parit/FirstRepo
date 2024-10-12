0708b435de8ec5184dc838863304c48948d65196
hp <hp@DESKTOP-QD6Q4G9>
2024-10-12 18:04:45 +0530

    Test

---
+++ src/main/java/WebAppTesting/Practice/OpenYouTube.java
@@ -0,0 +1,18 @@
+package WebAppTesting.Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpenYouTube {
+
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com");
+		Thread.sleep(3000);
+	}
+
+}
